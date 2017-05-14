15479c05c52e60b81ffe18a95c71e8ddcd28ecca
lmorl01 <lmorl01@mail.bbk.ac.uk>
2017-05-14 11:07:14 +0100

    Changes to be committed: 	new file:   TPRTools.pm

---
+++ TPRTools.pm
@@ -0,0 +1,18 @@
+#################################################################################
+# David Morley, MSc Bioinformatics 2015-2017
+# MSc Project: Origin & Evolution of TPR Domains
+# Author: David Morley
+# Package Name: TPRTools.pm
+# Version: 0001 (13/05/17 18:09)
+#
+# Purpose: 	A package of tools developed for use in the Origin & Evolution of 
+#			TPR Domains project#
+#
+# Assumptions:
+#
+# Strategy:
+#
+# Error Behaviour:
+# 1.
+#
+#####################################################################################
