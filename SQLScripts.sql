94ac3294b3e1eb7da74c4fd712d64d01696f75f5
lmorl01 <lmorl01@users.noreply.github.com>
2017-08-05 17:15:09 +0100

    SQL Scripts

--- SQLScripts.sql
+++ SQLScripts.sql
@@ -31,3 +31,21 @@ GROUP BY
 R.resultId, E.queryPdb, R.resultPdb, TPR.TPROrdinal, TPR.endResidue
 ORDER BY 
 E.queryPdb, R.resultPdb, TPR.TPROrdinal;
+
+-- SQL to select start and end TPR residue numbers for results
+
+SELECT R.resultId, E.queryPdb, R.resultPdb, TPR.TPROrdinal, TPR.startResidue AS 'StartResidueQuery', TPR.endResidue AS 'EndResidueQuery', MIN(A.resultResidueNo) AS 'StartResidueResult', MAX(A.resultResidueNo) AS 'EndResidueResult'
+FROM 
+Results R, Experiment E, ParameterSet P, TPR, Alignment A 
+WHERE
+TPR.regionId = E.regionId and R.experimentId = E.experimentId and A.resultId = R.resultId 
+AND 
+A.queryResidueNo >= TPR.startResidue and A.queryResidueNo <= TPR.endResidue
+AND 
+E.superseded is null and TPR.superseded is null
+GROUP BY
+R.resultId, E.queryPdb, R.resultPdb, TPR.TPROrdinal, TPR.startResidue, TPR.endResidue
+ORDER BY 
+E.queryPdb, R.resultPdb, TPR.TPROrdinal;
+
+
