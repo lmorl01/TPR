b5938e11d0903166e11ac6c078eab3025d74ea18
lmorl01 <lmorl01@users.noreply.github.com>
2017-08-07 13:39:37 +0100

    Parse alignment files and add details to the DB

--- addAlignments.pl
+++ addAlignments.pl
@@ -3,7 +3,8 @@
 # MSc Project: Origin & Evolution of TPR Domains
 # Author: David Morley
 # Script Name: addAlignments.pl
-# Version: 0001 (05/08/17)
+# Version: 	0001 (05/08/17)
+#			0002 (07/08/17) Close all file handles; pass all values in a single INSERT
 #
 # Purpose: 	Read a list of Result.resultId, Result.pdbText, Experiment.resultsLocation 
 #			entries exported from the DB and use this information to parse the
@@ -42,15 +43,18 @@ if (!(scalar @ARGV == 2)){
 my ($in, $out) = ($ARGV[0], $ARGV[1]);
 my $root = "\/d\/mw6\/u\/md003\/results\/";
 my ($prefix, $suffix) = ("\/dbsearch_CUSTOM_", ".xml.gz");
-my $count = 0;
+my $resultCount = 0;
+my $lineCount = 0;
  
 open(INFILE, $in)
       or die "Can't open file $in\n"; 
 open(OUTFILE, ">$out")
 	 or die "Can't create output file $out\n";	
+
+print OUTFILE "INSERT INTO Alignment (resultId, queryResidueNo, resultResidueNo) VALUES \n";
 	 
 while (my $line = <INFILE>) {
-	$count++;
+	$resultCount++;
 	my @results = split /,/, $line;
 	my $resultId = $results[0];
 	my $pdbText = $results[1];
@@ -62,10 +66,14 @@ while (my $line = <INFILE>) {
 	processAlignments($resultId, $alignFile);
 	print "Zipping $alignFile\n";
 	system("gzip $alignFile");
-	if ($count%100 == 0){
-		print "$count records processed\n";
+	if ($resultCount%100 == 0){
+		print "$resultCount records processed\n";
 	}
 }	 
+
+print OUTFILE ";";
+close(INFILE);
+close(OUTFILE) or die "Unable to close $out\n";
 
 sub processAlignments($$){
 	my $resultId = $_[0];
@@ -73,8 +81,14 @@ sub processAlignments($$){
 	if (open(IN, $path)){
 		while (my $line = <IN>){
 			if ($line =~ /pdbres1="(\d+)"\schain1="([A-Za-z0-9])"\spdbres2="(\d+)"\schain2="([A-Za-z0-9])"/){
-				print OUTFILE "INSERT INTO Alignment (resultId, queryResidueNo, resultResidueNo) VALUES ($resultId,$1,$3);\n";		
+				if ($lineCount == 0){
+					print OUTFILE "($resultId,$1,$3)";	
+					$lineCount++;
+				} else {
+					print OUTFILE ",\n($resultId,$1,$3)";		
+				}	
 			}
 		}
+		close(IN);
 	}
 }
