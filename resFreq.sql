1327c3e033422ec3d9016d986a08ac7571589c3c
lmorl01 <lmorl01@users.noreply.github.com>
2017-08-08 10:42:22 +0100

    Extract data for residue frequency analysis

---
+++ resFreq.sql
@@ -0,0 +1,17 @@
+-- Select residue alignment frequency data for query PDBs, cancatenating PDB Code, Chain and TPR Region
+-- Single block results
+-- Fields are those required for residue alignment frequency analysis
+
+SELECT 
+CONCAT(queryPdb, TPRR.chain, '_', TPRR.regionOrdinal) AS 'PDBChainRegion', A.queryResidueNo, COUNT(*) 
+FROM
+Results R, Alignment A, Experiment E, TPRRegion TPRR 
+WHERE 
+R.resultId = A.resultId and R.experimentId = E.experimentId and E.regionId = TPRR.regionId
+AND
+R.norm_rmsd < 0.24 and R.norm_score > 18 and R.probability < 0.004 and R.cov1 > 90 and R.blocks = 1 
+AND
+E.superseded IS NULL and TPRR.superseded IS NULL
+GROUP BY 
+'PDBChainRegion', A.queryResidueNo
+INTO OUTFILE '/d/user6/md003/Project/db/sqlout/resFreq_1Blk.csv' fields terminated by ',' lines terminated by '\n';
