da7ed065cd7cf1dd47b22d862dc9fa58bf0b6cf2
lmorl01 <lmorl01@users.noreply.github.com>
2017-08-12 12:55:53 +0100

    Get TTPR Motif Data

--- getTTPRMotifData.sql
+++ getTTPRMotifData.sql
@@ -1,13 +1,13 @@
 -- In development: Data needed to determine consensus TTPR Motif Alignment:
 
 SELECT 
-TTPR.ttprId, TTPR.pdbCode, TTPR.chain, TTPR.regionOrdinal, TTPR.tprOrdinal, TTPR.startMode, TTPR.endMode, A.resultResidueNo
+TTPR.ttprId, TTPR.pdbCode, TTPR.chain, TTPR.regionOrdinal, TTPR.tprOrdinal, TTPR.startMode, TTPR.endMode, A.resultResidueNo, E.queryPdb
 FROM 
-TentativeTPR TTPR, Results R, Alignment A
+TentativeTPR TTPR, Results R, Alignment A, Experiment E
 WHERE
-TTPR.pdbCode = R.resultPdb AND TTPR.chain = R.chain AND R.resultId = A.resultId
+TTPR.pdbCode = R.resultPdb AND TTPR.chain = R.chain AND R.resultId = A.resultId AND R.experimentId = E.experimentId
 AND
 A.resultResidueNo >= TTPR.startMode and A.resultResidueNo <= TTPR.endMode
 AND
-TTPR.ttprParamId = 2
+E.superseded IS NULL AND TTPR.ttprParamId = 2
 INTO OUTFILE '/d/user6/md003/Project/db/sqlout/ttprMotifData.csv' fields terminated by ',' lines terminated by '\n';
