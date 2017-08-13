3690f082e26a8997588a2c5737291a66c88d0120
lmorl01 <lmorl01@users.noreply.github.com>
2017-08-13 16:18:24 +0100

    SQL Scripts

--- SQLScripts.sql
+++ SQLScripts.sql
@@ -351,7 +351,16 @@ E.superseded IS NULL AND TTPR.ttprParamId = 2
 ORDER BY TTPR.pdbCode, TTPR.chain, TTPR.regionOrdinal, TTPR.tprOrdinal, A.resultResidueNo, M.motifNo
 INTO OUTFILE '/d/user6/md003/Project/db/sqlout/ttprMotifData.csv' fields terminated by ',' lines terminated by '\n';
 
+-- Extract contact data for TTPRs to perform contact analysis
 
+SELECT
+C.pdbCode, C.chain, C.residue1, C.residue2, S1.residue, S2.residue, M1.motifNo, M2.motifNo
+FROM
+Contact C, Sequence S1, Sequence S2, TTPRMotifAlignment M1, TTPRMotifAlignment M2
+WHERE 
+C.pdbCode = S.pdbCode and C.chain = S.chain and C.residue1 = S1.residueNo and C.residue2 = S2.residueNo and
+C.residue1 = M1.residueNo and C.residue2 = M2.residueNo
+INTO OUTFILE '/d/user6/md003/Project/db/sqlout/contacts.csv' fields terminated by ',' lines terminated by '\n';
 
 
 
---
+++ getTTPRMorifContacts.sql
@@ -0,0 +1,10 @@
+-- Extract contact data for TTPRs to perform contact analysis
+
+SELECT
+C.pdbCode, C.chain, C.residue1, C.residue2, S1.residue, S2.residue, M1.motifNo, M2.motifNo
+FROM
+Contact C, Sequence S1, Sequence S2, TTPRMotifAlignment M1, TTPRMotifAlignment M2
+WHERE 
+C.pdbCode = S.pdbCode and C.chain = S.chain and C.residue1 = S1.residueNo and C.residue2 = S2.residueNo and
+C.residue1 = M1.residueNo and C.residue2 = M2.residueNo
+INTO OUTFILE '/d/user6/md003/Project/db/sqlout/contacts.csv' fields terminated by ',' lines terminated by '\n';
