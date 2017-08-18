d3e7e5a11ac84a2aecbacd041442a0ffd96fdfaf
lmorl01 <lmorl01@users.noreply.github.com>
2017-08-18 22:25:16 +0100

    Get TTPR Contact/Motif Data

--- getTTPRMotifContacts.sql
+++ getTTPRMotifContacts.sql
@@ -1,11 +1,37 @@
 -- Extract contact data for TTPRs to perform contact analysis
 
-SELECT
-C.pdbCode, C.chain, C.residue1, C.residue2, S1.residue, S2.residue, M1.motifNo, M2.motifNo
+SELECT -- COUNT(*)
+TTPR.pdbCode, TTPR.tprOrdinal, TTPR.startMode, TTPR.endMode,
+C.residue1, C.residue2, C.helix1, C.helix2, S1.residue, S2.residue,
+M1.motifNo, M2.motifNo, TTPR1.tprOrdinal, TTPR2.tprOrdinal
 FROM
-Contact C, Sequence S1, Sequence S2, TTPRMotifAlignment M1, TTPRMotifAlignment M2
+TentativeTPR TTPR, Contact C, Sequence S1, Sequence S2, 
+TTPRMotifAlignment M1, TentativeTPR TTPR1,
+TTPRMotifAlignment M2, TentativeTPR TTPR2
 WHERE 
-C.pdbCode = S1.pdbCode and C.chain = S1.chain and C.pdbCode = S2.pdbCode and C.chain = S2.chain and 
-C.residue1 = S1.residueNo and C.residue2 = S2.residueNo and
-C.residue1 = M1.residueNo and C.residue2 = M2.residueNo
+TTPR.pdbCode = C.pdbCode and TTPR.chain = C.chain and
+C.residue1 = S1.residueNo and TTPR.pdbCode = S1.pdbCode and TTPR.chain = S1.chain and
+C.residue2 = S2.residueNo and TTPR.pdbCode = S2.pdbCode and TTPR.chain = S2.chain
+
+-- Limit to contacts where at least one is in the TPR
+AND
+((TTPR.startMode <= C.residue1 and C.residue1 <= TTPR.endMode) OR (TTPR.startMode <= C.residue2 and C.residue2 <= TTPR.endMode))
+
+-- Join the first residue to the motif via the appropriate TTPR
+AND
+TTPR1.pdbCode = C.pdbCode and TTPR1.chain = C.chain
+AND 
+M1.ttprId = TTPR1.ttprId and M1.residueNo = C.residue1
+
+-- Join the second residue to the motif via the appropriate TTPR
+AND
+TTPR2.pdbCode = C.pdbCode and TTPR2.chain = C.chain
+AND
+M2.ttprId = TTPR2.ttprId and M2.residueNo = C.residue2
+
+AND
+-- TTPR.pdbCode = '1a17' and 
+TTPR.ttprParamId = 2
+-- GROUP BY TTPR.tprOrdinal
+ORDER BY TTPR.pdbCode, TTPR.tprOrdinal
 INTO OUTFILE '/d/user6/md003/Project/db/sqlout/contacts.csv' fields terminated by ',' lines terminated by '\n';
